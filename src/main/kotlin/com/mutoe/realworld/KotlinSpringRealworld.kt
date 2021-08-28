6b6d7491bdfdc41fd58fb734ccc55272b0828dfe
mutoe <mutoe@foxmail.com>
2021-08-29 00:10:10 +0800

    chore: init spring boot

---
+++ src/main/kotlin/com/mutoe/realworld/KotlinSpringRealworld.kt
@@ -0,0 +1,11 @@
+package com.mutoe.realworld
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringRealworld
+
+fun main(args: Array<String>) {
+    runApplication<KotlinSpringRealworld>(*args)
+}
---
+++ src/test/kotlin/com/mutoe/realworld/KotlinSpringRealworldTests.kt
@@ -0,0 +1,13 @@
+package com.mutoe.realworld
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringRealworldTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
